0992f46f40ccc44c987d50c64c244761b991dce7
Tommaso <cenedesetommaso@gmail.com>
2023-04-27 17:29:01 +0200

    first commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// console.log("Hello World!")
+// console.log("🚀 This is my first Node Project")
+let myInterface = {
+    myNumber: 10,
+    myString: 'Hello World!'
+};
+console.log(`myNumber = ${myInterface.myNumber}`);
+console.log(`myString = ${myInterface.myString}`);
---
+++ main.ts
@@ -0,0 +1,22 @@
+// console.log("Hello World!")
+// console.log("🚀 This is my first Node Project")
+
+// let myVar: number = 3;
+// myVar += 10; //13
+
+// console.log(`myVar = ${myVar}`);
+
+
+
+interface MyInterface {
+    myNumber: Number;
+    myString: String;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: 'Hello World!'
+}
+
+console.log(`myNumber = ${myInterface.myNumber}`);
+console.log(`myString = ${myInterface.myString}`);
